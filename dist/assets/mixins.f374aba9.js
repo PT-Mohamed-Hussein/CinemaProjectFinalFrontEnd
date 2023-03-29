2a7b40b79260868b0136dda44fe3a103c57fe99a
Diesel <pt.mohamed.ahmed@gmail.com>
2023-03-30 01:51:26 +0200

    adding the dist

--- dist/assets/mixins.f374aba9.js
+++
@@ -1,1 +0,0 @@
-const s={data(){return{passwordtype:"password",passwordconfirmationtype:"password"}},methods:{changePassType(){this.passwordtype=="password"?this.passwordtype="text":this.passwordtype="password"},changePassConfirmationType(){this.passwordconfirmationtype=="password"?this.passwordconfirmationtype="text":this.passwordconfirmationtype="password"}}};export{s as m};
